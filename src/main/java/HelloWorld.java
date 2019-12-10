1784ecc8b050f25790c90bdbdb612dba345240ac
deepusingh195 <58683435+deepusingh195@users.noreply.github.com>
2019-12-10 17:37:38 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World good work Nausan good work ");
+		get("/hello", (req, res) -> "Hello World auto after build ");
 	}
 }
